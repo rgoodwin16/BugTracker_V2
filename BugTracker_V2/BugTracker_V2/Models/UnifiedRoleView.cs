81a42f74bddf94961fcdb581cf69db077e7a6564
Ray Goodwin <rgoodwin16@outlook.com>
2015-08-01 14:00:57 -0400

    8/1/15
    Working on adding users to roles

---
+++ BugTracker_V2/BugTracker_V2/Models/UnifiedRoleView.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BugTracker_V2.Models
+{
+    public class UnifiedRoleView
+    {
+        public string RoleId { get; set; }
+        public string RoleName { get; set; }
+        
+        public MultiSelectList AddedUsers { get; set; }
+        public MultiSelectList DeletedUsers { get; set; }
+        public MultiSelectList Users { get; set; }
+
+        public string[] AddedSelect { get; set; }
+        public string[] DeletedSelect { get; set; }
+        public string[] Selected { get; set; }
+        public Project Project { get; set; }
+    }
+}
